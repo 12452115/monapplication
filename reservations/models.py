3f8317d4e00961d7820ffdac4bf9093a8f29b8f0
khames <lynda.khames@yahoo.fr>
2024-11-16 21:53:11 +0100

    Modifications du modèle et ajout des migrations

--- reservations/models.py
+++ reservations/models.py
@@ -87,6 +87,7 @@ class Offer(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    max_people=models.IntegerField()
 
     def __str__(self):
         return self.name
